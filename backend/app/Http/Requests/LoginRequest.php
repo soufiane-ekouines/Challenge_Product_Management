4fe08d252c94676e8172668b848ebcebdb361591
soufiane-ekouines <soufianeekouines@gmail.com>
2023-09-23 22:46:49 +0100

    create the logic of authentication

---
+++ backend/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class LoginRequest extends FormRequest
+{
+    protected function failedValidation(Validator $validator)
+    {
+        $response = new JsonResponse([
+            'message' => 'The given data was invalid.',
+            'errors' => $validator->errors(),
+        ], 422);
+
+        throw new HttpResponseException($response);
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required'
+        ];
+    }
+}
---
+++ frontend/product_Management_client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import Navbar from '@/components/Navbar.vue';
+
+</script>
+
+<template>
+  <div w-full>
+    <Navbar />
+  </div>
+  <RouterView />
+</template>
